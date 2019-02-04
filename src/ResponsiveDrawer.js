4b24fe6a003aaddd2feca2b12ac7646668a4ce0b
Hafid Afridian <afridianhafid@gmail.com>
2019-02-04 21:06:49 +0700

    add mobile stepper

--- src/ResponsiveDrawer.js
+++ src/ResponsiveDrawer.js
@@ -16,6 +16,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import SwipeableTextMobileStepper from './SwipeableTextMobileStepper';
 
 const drawerWidth = 240;
 
@@ -134,6 +135,7 @@ class ResponsiveDrawer extends React.Component {
                 </nav>
                 <main className={classes.content}>
                     <div className={classes.toolbar} />
+                    <SwipeableTextMobileStepper />
                     <Typography paragraph>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                     </Typography>
