90e47e30bf31617c0d5db398e4d9a26fdaef817b
Hafid Afridian <afridianhafid@gmail.com>
2019-02-06 10:54:04 +0700

    add axios

--- src/SwipeableTextMobileStepper.js
+++ src/SwipeableTextMobileStepper.js
@@ -9,31 +9,11 @@ import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import axios from 'axios';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const tutorialSteps = [
-    {
-        label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bird',
-        imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Bali, Indonesia',
-        imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-    },
-    {
-        label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-        imgPath: 'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-        label: 'Goč, Serbia',
-        imgPath: 'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-];
+const PATH_BASE = 'https://api.thecatapi.com/v1/images/search?limit=10';
 
 const styles = theme => ({
     root: {
@@ -59,6 +39,23 @@ const styles = theme => ({
 class SwipeableTextMobileStepper extends React.Component {
     state = {
         activeStep: 0,
+        catsList: []
+    };
+
+    componentDidMount() {
+        this.fetchCatsListData();
+    }
+
+    fetchCatsListData = () => {
+        axios(`${PATH_BASE}`)
+            .then(result => this.setCatsListData(result.data))
+            .catch(error => this.setState({ error }));
+    };
+
+    setCatsListData = (result) => {
+        this.setState({
+            catsList: result,
+        });
     };
 
     handleNext = () => {
@@ -80,12 +77,13 @@ class SwipeableTextMobileStepper extends React.Component {
     render() {
         const { classes, theme } = this.props;
         const { activeStep } = this.state;
-        const maxSteps = tutorialSteps.length;
+        const maxSteps = this.state.catsList.length;
 
         return (
+            maxSteps === 0 ? <div></div> :
             <div>
                 <Paper square elevation={0} className={classes.header}>
-                    <Typography>{tutorialSteps[activeStep].label}</Typography>
+                    <Typography>{this.state.catsList[activeStep].id}</Typography>
                 </Paper>
                 <AutoPlaySwipeableViews
                     axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
@@ -93,10 +91,10 @@ class SwipeableTextMobileStepper extends React.Component {
                     onChangeIndex={this.handleStepChange}
                     enableMouseEvents
                 >
-                    {tutorialSteps.map((step, index) => (
-                        <div key={step.label}>
+                    {this.state.catsList.map((step, index) => (
+                        <div key={step.id}>
                             {Math.abs(activeStep - index) <= 2 ? (
-                                <img className={classes.img} src={step.imgPath} alt={step.label} />
+                                <img className={classes.img} src={step.url} alt={step.id} />
                             ) : null}
                         </div>
                     ))}
